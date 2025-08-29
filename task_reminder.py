c30c81aa14a11fca0ff1e48fcff828b2f611c456
Henry Gao <77124321+Henry-Gao-2004@users.noreply.github.com>
2025-08-28 23:44:21 -0400

    Update task_reminder.py

--- task_reminder.py
+++ task_reminder.py
@@ -1,1 +1,8 @@
+from collections import namedtuple
+
 # record the todo tasks and then tell the user what task to do next based on priority and deadline.
+class Task:
+    def __init__(self, name: str, time: str, priority: int):
+        self.name = name
+        self.time = time
+        self.priority = priority
