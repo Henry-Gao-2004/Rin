69dfb498a3c1e0d3f8233fc56ea1b00dd5859eed
Henry Gao <77124321+Henry-Gao-2004@users.noreply.github.com>
2025-09-06 21:31:48 -0400

    Write test for response_to_text

--- utils/utils_test.py
+++ utils/utils_test.py
@@ -6,6 +6,12 @@ def prompt_gpt_test():
     response = prompt_gpt(prompt)
     print(response)
 
+def response_to_text_test():
+    prompt = "What is the capital of France?"
+    response = prompt_gpt(prompt)
+    text_response = response_to_text(response)
+    print(text_response)
+
 
 if __name__ == "__main__":
     prompt_gpt_test()
