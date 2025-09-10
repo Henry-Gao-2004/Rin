9bd1f0b830bd721ab2b5aa33fc6051c6a70253c2
Henry Gao <77124321+Henry-Gao-2004@users.noreply.github.com>
2025-09-09 20:51:52 -0400

    Add comments for utils functions

--- utils/utils.py
+++ utils/utils.py
@@ -4,6 +4,12 @@ import ndjson
 base_url = "http://localhost:11434"
 
 def prompt_gpt(prompt: str, options = {}) -> requests.Response:
+    """
+    use ollama to access the gpt-oss:20b model deployed locally
+    prompt: str, the prompt to send to the model
+    options: dict, additional options to pass to the model
+    return: requests.Response, the response from the model
+    """
     url = base_url+"/api/chat"
     payload = {
         "model": "gpt-oss:20b",
@@ -19,6 +25,11 @@ def prompt_gpt(prompt: str, options = {}) -> requests.Response:
     return response
 
 def response_to_text(response: requests.Response) -> str:
+    """
+    Convert the response from the model output to text.
+    response: requests.Response, the response from the model
+    return: str, the text output from the model
+    """
     if response.status_code != 200:
         return False, f"Error: {response.status_code}, {response.text}"
     try:
