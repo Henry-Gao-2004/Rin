63283d203c92e1a4815de78cd88b4e9a621f45cd
Henry Gao <77124321+Henry-Gao-2004@users.noreply.github.com>
2025-09-13 23:21:26 -0400

    Write comments for cover letter editor

--- cover_letter_editor.py
+++ cover_letter_editor.py
@@ -1,6 +1,12 @@
 from utils import prompt_gpt
 
 def generate_cover_letter(company: str, position: str):
+    """
+    Generate a cover letter for a given company and position using a template and resume.
+    company: str, the name of the company
+    position: str, the position to apply for
+    return: tuple(bool, str), (status, cover_letter or error message)
+    """
     with open("data/cover_letter_template.txt", "r") as file:
         cover_letter_template = file.read()
     with open("data/resume.txt", "r") as file:
